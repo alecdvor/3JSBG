91c74ef9d1f529e88831f6fd0ec0e732f2491ffa
Alec <56179545+alecdvor@users.noreply.github.com>
2025-09-06 18:26:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const clock = new THREE.Clock();
 const loadedScenes = {};
 const defaultConfigs = {}; // Store default configs for comparison
 
-const sceneFiles = ['deepSpace.js', 'lavaLamp.js', 'smokeyLights.js'];
+const sceneFiles = ['deepSpace.js', 'lavaLamp.js', 'scenes/smokeyLights.js'];
 
 async function init() {
     scene = new THREE.Scene();
