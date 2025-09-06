3fccb52c5fa1b44d1f64a9ba65eaa017100db593
Alec <56179545+alecdvor@users.noreply.github.com>
2025-09-06 18:29:56 -0400

    Update smokeyLights.js

--- scenes/smokeyLights.js
+++ scenes/smokeyLights.js
@@ -26,7 +26,7 @@ function createSmokeTexture() {
     return new THREE.CanvasTexture(canvas);
 }
 
-const smokeyLights = {
+const smokeyLights = { 
     // Scene configuration object
     config: {
         particleCount: 700,
